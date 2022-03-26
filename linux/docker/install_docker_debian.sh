3317b36abf75d7e2bde48880f23b762e938358d8
Cliff Wu <z800201002005@gmail.com>
2022-03-26 23:03:57 +0800

    Add a script to install Docker in Debian-based

---
+++ linux/docker/install_docker_debian.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Set up the repository
+sudo apt-get update
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+# Install Docker Engine
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Manage Docker as a non-root user
+sudo gpasswd -a $USER docker
