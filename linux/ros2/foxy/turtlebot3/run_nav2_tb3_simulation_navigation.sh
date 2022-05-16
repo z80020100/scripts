022218bad420480a209370562d3651a9f1374ec2
Cliff Wu <z800201002005@gmail.com>
2022-05-16 14:44:27 +0900

    Add scripts to start navigation while mapping by Nav2 (Foxy)

---
+++ linux/ros2/foxy/turtlebot3/run_nav2_tb3_simulation_navigation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+BASEDIR=$(dirname "$0")
+TB3_ENV_SETUP_FILE_PATH=$BASEDIR/envsetup.sh
+
+source $TB3_ENV_SETUP_FILE_PATH
+
+# https://navigation.ros.org/getting_started/index.html#running-the-example
+function main()
+{
+  export TURTLEBOT3_MODEL=waffle
+  export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:$TB3_ROS2_WS_DIR/install/turtlebot3_gazebo/share/turtlebot3_gazebo/models
+  ros2 launch nav2_bringup tb3_simulation_launch.py
+}
+
+echo "NOTICE: it will take a long time to start Gazebo and has a chance of failing"
+read -p "Press [Enter] to start or [Ctrl-C] to cancel..."
+main
