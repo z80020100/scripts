8fffed1dfb96ac10417510fa3e4fc9805e1bf469
Cliff Wu <z800201002005@gmail.com>
2022-05-18 11:37:35 +0900

    Revise repos and script file for building TurtleBot3 with ROS 2 Galactic

--- linux/ros2/galactic/turtlebot3/build_turtlebot3_galactic.sh
+++ linux/ros2/galactic/turtlebot3/build_turtlebot3_galactic.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 BASEDIR=$(dirname "$0")
 ROS2_DIR=~/ros2_galactic
-TB3_ROS2_WS_DIR=~/tb3_ws2
-TB3_REPOS_FILE_NAME=turtlebot3.repos
+TB3_ROS2_WS_DIR=~/tb3_galactic_ws2
+TB3_REPOS_FILE_NAME=turtlebot3_galactic.repos
 TB3_REPOS_FILE_PATH=$BASEDIR/$TB3_REPOS_FILE_NAME
 TB3_BUILD_SCRIPT_FILE_NAME=$(basename "$0")
+MAP_DIR=$TB3_ROS2_WS_DIR/map
+NAV2_BUILDIN_MAPS_DIR=$TB3_ROS2_WS_DIR/src/ros-planning/navigation2/nav2_bringup/bringup/maps
 
 function check_result()
 {
@@ -19,13 +21,23 @@ function check_result()
   fi
 }
 
-function get_tb3_ros2()
+function create_resouces()
 {
   mkdir -p $TB3_ROS2_WS_DIR/src
   cp $TB3_REPOS_FILE_PATH $TB3_ROS2_WS_DIR
   cp $BASEDIR/*.sh $TB3_ROS2_WS_DIR
+}
+
+function get_tb3_ros2()
+{
   cd $TB3_ROS2_WS_DIR
   vcs import src < $TB3_REPOS_FILE_NAME
+}
+
+function copy_maps()
+{
+  mkdir -p $MAP_DIR
+  cp $NAV2_BUILDIN_MAPS_DIR/* $MAP_DIR
 }
 
 function build_tb3_ros2()
@@ -37,7 +49,9 @@ function build_tb3_ros2()
 
 function main()
 {
+  create_resouces
   check_result get_tb3_ros2
+  # check_result copy_maps
   check_result build_tb3_ros2
 }
 
