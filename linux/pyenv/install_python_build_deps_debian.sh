8c1b27d7c051bc57a81359d7e0e990a33485fbde
Cliff Wu <z800201002005@gmail.com>
2023-04-21 10:47:05 +0900

    Add script to install python build dependencies

---
+++ linux/pyenv/install_python_build_deps_debian.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Install python build dependencies
+# https://github.com/pyenv/pyenv/wiki#suggested-build-environment
+sudo apt update
+sudo apt install -y build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev curl \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
