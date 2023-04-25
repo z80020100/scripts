7c1a106563d2c1528d179c5f804eca4f88aed753
Cliff Wu <z800201002005@gmail.com>
2023-04-25 13:00:01 +0900

    Add restarting shell command to script for set Pyenv

--- linux/pyenv/set_env_var_pyenv_bash.sh
+++ linux/pyenv/set_env_var_pyenv_bash.sh
@@ -5,3 +5,4 @@ echo '# Pyenv' >> $HOME/.bashrc
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $HOME/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.bashrc
 echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
+exec "$SHELL"
