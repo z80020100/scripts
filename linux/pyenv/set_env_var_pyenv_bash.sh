fdd1a507b1a476759aa7308b29361725982393fd
Cliff Wu <z800201002005@gmail.com>
2023-04-25 12:44:54 +0900

    Add scripts to set environment for Pyenv on Bash

---
+++ linux/pyenv/set_env_var_pyenv_bash.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> $HOME/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> $HOME/.bashrc
+echo 'eval "$(pyenv init -)"' >> $HOME/.bashrc
