69f9224dfe62b0875d4497840ed778deecc97507
Cliff Wu <z800201002005@gmail.com>
2023-06-03 00:44:14 +0900

    Add quilt to development tools

--- linux/tools/install_development_tools_debian.sh
+++ linux/tools/install_development_tools_debian.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y build-essential git tig vim
+sudo apt update && sudo apt install -y build-essential git quilt tig vim
 sudo apt install -y libelf-dev
