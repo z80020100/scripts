5cd4ca3ab31f92e86da2d5945d03d2e54a564c6a
Cliff Wu <z800201002005@gmail.com>
2023-06-08 15:12:00 +0900

    Add a PowerShell script to show info about all network adapters on your computer

---
+++ win/powershell/os/Get_NetAdapter.ps1
@@ -0,0 +1,3 @@
+# https://learn.microsoft.com/en-us/previous-versions/windows/desktop/legacy/hh968170(v=vs.85)
+# https://learn.microsoft.com/en-us/powershell/module/netadapter/get-netadapter
+Get-NetAdapter | SELECT *
