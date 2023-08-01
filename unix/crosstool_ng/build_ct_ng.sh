d965fa9f65a43aea916814d24fc271b6a601d1da
Cliff Wu <z800201002005@gmail.com>
2023-08-01 18:16:34 +0900

    Support to build and install crosstool-NG on macOS

--- unix/crosstool_ng/build_ct_ng.sh
+++ unix/crosstool_ng/build_ct_ng.sh
@@ -2,6 +2,15 @@
 
 # https://github.com/crosstool-ng/crosstool-ng/blob/master/.github/workflows/continuous-integration-workflow.yml
 
+OS=$(uname -s)
+ARCH=$(uname -m)
+DARWIN_LIB_PATH_ARM64=/opt/homebrew/opt/
+DARWIN_LIB_PATH_X86_64=/usr/local/opt/
+if [ "$ARCH" == "arm64" ]; then
+  DARWIN_LIB_PATH=$DARWIN_LIB_PATH_ARM64
+else # x86_64
+  DARWIN_LIB_PATH=$DARWIN_LIB_PATH_X86_64
+fi
 BUILD_DIR=$HOME/build
 CT_NG_DIR=$BUILD_DIR/crosstool-ng
 # Load CT_NG_INSTALL_DIR from envsetup.sh
@@ -20,6 +29,11 @@ function check_result() {
 }
 
 function build_ct_ng() {
+  if [ "$OS" == "Darwin" ]; then
+    export PATH="$PATH:$DARWIN_LIB_PATH/binutils/bin"
+    export CPPFLAGS="-I$DARWIN_LIB_PATH/ncurses/include -I$DARWIN_LIB_PATH/gettext/include"
+    export LDFLAGS="-L$DARWIN_LIB_PATH/ncurses/lib -L$DARWIN_LIB_PATH/gettext/lib"
+  fi
   cd $CT_NG_DIR
   ./bootstrap
   ./configure --prefix=$CT_NG_INSTALL_DIR
