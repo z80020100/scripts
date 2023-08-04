e256aa4c674a377c96fb4be4321c0c2639278ef0
Cliff Wu <z800201002005@gmail.com>
2023-08-04 14:36:14 +0900

    Support to run the specific the version of official Docker image of Ubuntu

--- linux/docker/run_ubuntu_offical.sh
+++ linux/docker/run_ubuntu_offical.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 # https://hub.docker.com/_/ubuntu
 
-docker run -it ubuntu:22.04 /bin/bash
+DEFAULT_VERSION="latest"
+VERSION=${1:-$DEFAULT_VERSION}
+
+function usage() {
+  echo "Usage: $0 [version]"
+  echo "  version: Ubuntu version, default: $DEFAULT_VERSION"
+  echo "Example: $0 22.04"
+}
+
+function run() {
+  docker run -it ubuntu:$VERSION /bin/bash
+}
+
+function main() {
+  if [ "$VERSION" == "-h" ] || [ "$VERSION" == "--help" ]; then
+    usage
+    exit 1
+  fi
+  run
+}
+
+main
