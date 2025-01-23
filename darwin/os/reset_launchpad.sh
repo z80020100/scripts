030f1a05eaf9876ef8c1f6a340f1aef79fe5ef16
Cliff Wu <z800201002005@gmail.com>
2025-01-23 13:42:09 +0900

    Fix the script of reset launchpad for macOS 15.2

--- darwin/os/reset_launchpad.sh
+++ darwin/os/reset_launchpad.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-defaults write com.apple.dock ResetLaunchPad -bool true; killall Dock
+defaults write com.apple.dock ResetLaunchPad -bool true
+# Provide by DeepSeek
+trash /private$(getconf DARWIN_USER_DIR)com.apple.dock.launchpad
+killall Dock
