07484fc809036e724cadce45ba2c4c3fcca95d5b
Cliff Wu <z800201002005@gmail.com>
2025-08-03 18:01:31 +0900

    Fix install script to add pyenv comment and remove bash sourcing of zshrc

--- darwin/pyenv/install_pyenv.sh
+++ darwin/pyenv/install_pyenv.sh
@@ -6,9 +6,10 @@
 brew install pyenv
 
 # Add pyenv to shell configuration
+echo '' >>~/.zshrc
+echo '# pyenv' >>~/.zshrc
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >>~/.zshrc
 echo '[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"' >>~/.zshrc
 echo 'eval "$(pyenv init - zsh)"' >>~/.zshrc
 
-# Source the updated configuration
-source ~/.zshrc
+echo "Please restart your terminal or run 'source ~/.zshrc' in a zsh shell to activate pyenv."
