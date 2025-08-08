55012f8c765d05b26be69715620d4fa3b7b3ba1a
Cliff Wu <z800201002005@gmail.com>
2025-08-08 23:28:50 +0900

    Fix formatting in HackMD CLI uninstall scripts

--- common/hackmd/uninstall_codimd_cli.sh
+++ common/hackmd/uninstall_codimd_cli.sh
@@ -9,7 +9,7 @@ npm uninstall -g @hackmd/codimd-cli
 # Check if uninstallation was successful
 if ! command -v codimd-cli &>/dev/null; then
     echo "@hackmd/codimd-cli uninstalled successfully!"
-    
+
     # Ask user if they want to remove config directory
     read -p "Do you want to remove the config directory ~/.codimd? (y/N): " -n 1 -r
     echo
